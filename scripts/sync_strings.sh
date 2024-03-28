07f7b9d9c3fe916161ba7464d197a686f0514ec6
PKEuS <philipp@pkeus.de>
2024-03-28 20:04:40 +0100

    Rewrote sync_strings.sh: - Avoid using intltool-extract and use xgettext directly to extract strings from gtkbuilder files. This avoids creating dozens of temporary files - Sort results to enforce stable ordering in the result file
    Updated POTFILES.in and RabbitVCS.pot again.

--- scripts/sync_strings.sh
+++ scripts/sync_strings.sh
@@ -1,29 +1,20 @@
 #!/bin/sh
 
-# Sync our translation template file with the actual strings
-# 1. Extracts strings from gtkbuilder xml files into .h files
-# 2. Extracts gettext strings from py files
-# 3. Deletes the glade .h files
+cd ../rabbitvcs
 
-cd ..
+# Find files from gtkbuilder (.xml)
+find . -type f | grep \.xml | grep -v \.svn | sort > ../POTFILES.glade.in
+sed -i 's|\.\/||g' ../POTFILES.glade.in
 
-for i in `find . | grep '\.xml' | grep -v '\.svn' | grep -v '\.xml\.h'`;
-do
-	intltool-extract --type=gettext/glade $i
-done
+# Find python files (.py)
+echo "util/helper.py" > ../POTFILES.py.in
+find . -type f | egrep '(ui)' | grep \.py | grep -v \.svn | grep -v \.pyc | sort >> ../POTFILES.py.in
+sed -i 's|\.\/||g' ../POTFILES.py.in
 
-cd rabbitvcs
+# Extract gettext strings
+xgettext -L Python --keyword=_ --keyword=N_ -o ../po/RabbitVCS.pot -f ../POTFILES.py.in
+xgettext -j -L Glade -o ../po/RabbitVCS.pot -f ../POTFILES.glade.in
 
-echo "util/helper.py" > POTFILES.in
-find . -type f | egrep '(ui|.xml)' | grep -v \.svn | grep -v \.h | grep -v \.pyc >> POTFILES.in 
-sed -i 's|\.xml|.xml.h|g' POTFILES.in
-sed -i 's|\.\/||g' POTFILES.in
-
-mv POTFILES.in ../po/POTFILES.in
-
-xgettext -L Python --keyword=_ --keyword=N_ -o ../po/RabbitVCS.pot -f ../po/POTFILES.in
-
-for i in `find . | grep '\.xml\.h' | grep -v '\.svn'`;
-do
-	rm -f $i
-done
+# Cleanup
+cat ../POTFILES.py.in ../POTFILES.glade.in > ../po/POTFILES.in
+rm ../POTFILES.py.in ../POTFILES.glade.in
